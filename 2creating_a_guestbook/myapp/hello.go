63207f43a9028d32084cb1f9e43501c186c8e049
cowsys <cowsys@gmail.com>
2016-01-12 00:59:22 +0900

    add

---
+++ 2creating_a_guestbook/myapp/hello.go
@@ -0,0 +1,33 @@
+package hello
+
+import (
+	"fmt"
+	"net/http"
+
+	"appengine"
+	"appengine/user"
+)
+
+func init() {
+	http.HandleFunc("/", handler)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	// セッションのコンテキストオブジェクト生成
+	c := appengine.NewContext(r)
+	u := user.Current(c)
+
+	if u == nil {
+		url, err := user.LoginURL(c, r.URL.String())
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Location", url)
+		w.WriteHeader(http.StatusFound)
+		return
+	}
+
+	fmt.Fprintf(w, "Hello, %v", u)
+}
